254f54727c34248f1836c3e286b5b7a09b7bc0e3
LorealCI <loreal1806@gmail.com>
2025-03-05 23:08:24 +0000

    added task and category model

--- taskmaster/settings.py
+++ taskmaster/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 
 from pathlib import Path
 import os
+import dj_database_url
 
 env_path = os.path.join(os.path.dirname(__name__), 'env.py')
 if os.path.exists(env_path):
@@ -28,7 +29,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
@@ -79,11 +80,15 @@ WSGI_APPLICATION = 'taskmaster.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+# DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.sqlite3',
+#        'NAME': BASE_DIR / 'db.sqlite3',
+#    }
+# }
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(default=os.getenv('DATABASE_URL'))
 }
 
 
--- tasks/models.py
+++ tasks/models.py
@@ -1,3 +1,26 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Category(models.Model):
+	name = models.CharField(max_length=50, unique=True)
+
+	def __str__(self):
+		return self.name
+
+# a model for Task to store task title, due date,
+# task completed status, and category.
+# task title is no longer than 100 characters,
+# completed status is False by default
+# and category is a foreign key to Category.
+
+
+class Task(models.Model):
+	title = models.CharField(max_length=100)
+	due_date = models.DateField()
+	completed = models.BooleanField(default=False)
+	category = models.ForeignKey(Category, on_delete=models.CASCADE)
+
+	def __str__(self):
+		return self.title
